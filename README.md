e60d9de8af2f8542a3ab505e7f9e67d127006b63
fitroh <fitrohiza@gmail.com>
2023-03-27 10:41:33 +0700

    Add Readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+#Landing Page NutriSense
+
+Ini merupakan landing page Aplikasi NutriSense
--- script.js
+++ script.js
@@ -9,4 +9,12 @@ window.addEventListener('scroll', function() {
     }
   });
 
-  console.log ('hai')
+  const navLinks = document.querySelectorAll("nav li a");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", function() {
+      navLinks.forEach(link => link.classList.remove("active"));
+      this.classList.add("active");
+    });
+  });
+  
