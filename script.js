ca533a74b26823139f6b26a0ee2b0094fad2e42a
fitroh <fitrohiza@gmail.com>
2023-04-08 10:23:32 +0700

    update about

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('navbar');
-    const scrollPosition = window.scrollY;
-  
-    if (scrollPosition > 0) {
-      navbar.style.backgroundColor = '#ffffff'; // ubah warna latar belakang menjadi putih
-    } else {
-      navbar.style.backgroundColor = 'transparent'; // ubah warna latar belakang menjadi transparan
-    }
-  });
-
-  const navLinks = document.querySelectorAll("nav li a");
-
-  navLinks.forEach(link => {
-    link.addEventListener("click", function() {
-      navLinks.forEach(link => link.classList.remove("active"));
-      this.classList.add("active");
-    });
-  });
-  
+function popupRun(){
+    window.alert("I'm Sorry, This Page is COOMING SOON");
+}
