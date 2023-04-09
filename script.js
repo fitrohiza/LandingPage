ea5abfa80d7ddbd2244db59868f1abc6c11ee375
fitroh <fitrohiza@gmail.com>
2023-04-09 12:34:05 +0700

    Add Section Article

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function popupRun(){
-    window.alert("I'm Sorry, This Page is COOMING SOON");
+function popupRun() {
+  window.alert("I'm Sorry, This Page is COOMING SOON");
 }
+
+//login function popup
+let modal = document.getElementById("loginModal");
+let btn = document.getElementById("loginBtn");
+let span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
